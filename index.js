0fd9f7dfd79e1f73838fef79be41e198ee907d41
DisEzMike <68768636+DisEzMike@users.noreply.github.com>
2021-12-26 19:17:56 +0700

    make it better

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
-const request = require('request');
-const dotenv = require('dotenv');
+const request = require("request");
+var express = require("express");
+var dotenv = require("dotenv");
+var bodyParser = require("body-parser");
+var app = express();
 dotenv.config();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-var url_line_notification = "https://notify-api.line.me/api/notify";
+var port = 9000;
 
-request({
-    method: 'POST',
-    uri: url_line_notification,
-    header: {
-        'Content-Type': 'multipart/form-data',
-    },
-    auth: {
-        bearer: process.env.TOKEN,
-    },
-    form: {
-        message: 'Test Message!'
-    },
-}, (err, httpResponse, body) => {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(body)
-    }
+app.get("/", function (req, res) {
+  res.json({ code: 405, message: "Method not allowed" });
 });
+
+app.post("/", function (req, res) {
+  try {
+    console.log("received request");
+    let message = req.body.message;
+    let tokenList = req.body.token;
+
+    //requset to line notify
+    tokenList.forEach(function (token) {
+      let options = {
+        method: "POST",
+        url: "https://notify-api.line.me/api/notify",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization: "Bearer " + token,
+        },
+        form: { message: message },
+      };
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        console.log(body);
+      });
+    });
+    res.json({ code: 200, message: "success" });
+  } catch (error) {
+    console.log("Request error");
+    res.json({ code: 500, message: "Internal server error" });
+  }
+});
+
+// start the server
+app.listen(port);
+console.log("Server started! At http://localhost:" + port);
