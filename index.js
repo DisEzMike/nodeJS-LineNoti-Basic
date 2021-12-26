f9820f52e8dacfa0c8890d8baef09fceb65e968d
DisEzMike <68768636+DisEzMike@users.noreply.github.com>
2021-12-26 20:01:58 +0700

    make it better

--- index.js
+++ index.js
@@ -2,10 +2,23 @@ const request = require("request");
 var express = require("express");
 var dotenv = require("dotenv");
 var bodyParser = require("body-parser");
-var app = express();
+const cors = require("cors");
 dotenv.config();
+
+var app = express();
+
+const corsOptions = {
+  origin: [
+    "http://localhost",
+    "https://sandbox.dexystore.me",
+    "https://dexystore.me",
+  ],
+  credentials: true,
+};
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 
 var port = process.env.PORT || 9000;
 
@@ -17,31 +30,46 @@ app.post("/", function (req, res) {
   try {
     console.log("received request");
     let message = req.body.message;
-    let tokenList = req.body.token;
+    let tokenList = req.body["token[]"];
+    let isArray = Array.isArray(tokenList);
 
     //requset to line notify
-    tokenList.forEach(function (token) {
-      let options = {
-        method: "POST",
-        url: "https://notify-api.line.me/api/notify",
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          Authorization: "Bearer " + token,
-        },
-        form: { message: message },
-      };
-      request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        console.log(body);
+    if (isArray) {
+      tokenList.forEach(function (token) {
+        sendLine(token, message);
       });
-    });
-    res.json({ code: 200, message: "success" });
+    } else {
+      sendLine(tokenList, message);
+    }
+
+    res.json({ code: 200, message: "success", data: tokenList });
   } catch (error) {
     console.log("Request error");
-    res.json({ code: 500, message: "Internal server error" });
+    res.json({
+      code: 500,
+      message: "Internal server error",
+      body: req.body,
+      error: error,
+    });
   }
 });
 
 // start the server
 app.listen(port);
 console.log("Server started! At http://localhost:" + port);
+
+function sendLine(token = "", message = "") {
+  let options = {
+    method: "POST",
+    url: "https://notify-api.line.me/api/notify",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Bearer " + token,
+    },
+    form: { message: message },
+  };
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+  });
+}
